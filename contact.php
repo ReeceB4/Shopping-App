a035ae64b345a696e46c90e511623537dcbbda65
ReeceB4 <bucktonreece@gmail.com>
2020-05-27 00:08:42 +0200

    contact form added

--- contact.php
+++ contact.php
@@ -39,7 +39,8 @@
 
 <div class="container1">
     <div class="contact-info">
-        <h3>Contact Us</h3><br>
+        <br>
+        <h2>Contact Us</h2><br>
         <p>
             <h6>Location: </h6> Sneaker Stash Store, 101 Shortmarket Street, Cape Town, South Africa, 8000 <br>
                                  021-559-7740 <br>
@@ -50,8 +51,53 @@
     <hr style="height:2px;border-width:0;color:gray;background-color:gray">
     <div class="directions">
         <h3>Visit us Instore</h3>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.8391787123473!2d18.413798315616596!3d-33.91953842912615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc67662b7f7dab%3A0xeaa6b193be496994!2s101%20Shortmarket%20St%2C%20Schotsche%20Kloof%2C%20Cape%20Town%2C%208001!5e0!3m2!1sen!2sza!4v1590527226543!5m2!1sen!2sza" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.8391787123473!2d18.413798315616596!3d-33.91953842912615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc67662b7f7dab%3A0xeaa6b193be496994!2s101%20Shortmarket%20St%2C%20Schotsche%20Kloof%2C%20Cape%20Town%2C%208001!5e0!3m2!1sen!2sza!4v1590527226543!5m2!1sen!2sza" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" id="map"></iframe>
+        
+        <div class="direction-info">
+            <h4>Cape Town</h4><br>
+            <u><h5>101 Shortmarket Street, CPT, 8000</h5></u><br>
+            <h5>021 559 7740 <br>
+                admin@sneakerstash.co.za</h5>
+            <h3> Store Hours</h3> <br>
+            <h5>MONDAY - FRIDAY: <B>9am - 5pm</B> <br>
+            SATURDAYS & SUNDAYS: <b>10am - 3pm</b> <br>
+            PUBLIC HOLIDAYS: <b>closed</b></h5>
+        </div>
     </div>
+    <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+
+        <h4><i>All queries can be submitted using the form below.</i></h4>
+
+    <form id="frmContact" action="" method="post">
+        <div id="mail-status"></div>
+            <div class="contact-row column-right">
+                <label style="padding-top: 20px;">Name</label> <span
+                    id="userName-info" class="info"></span><br /> <input
+                    type="text" name="userName" id="userName"
+                    class="demoInputBox">
+            </div>
+            <div class="contact-row column-right">
+                <label>Email</label> <span id="userEmail-info" class="info"></span><br />
+                <input type="text" name="userEmail" id="userEmail"
+                    class="demoInputBox">
+            </div>
+            <div class="contact-row">
+                <label>Phone</label> <span id="subject-info" class="info"></span><br />
+                <input type="text" name="subject" id="subject"
+                    class="demoInputBox">
+            </div>
+            <div>
+                <label>Message</label> <span id="content-info" class="info"></span><br />
+                <textarea name="content" id="content" class="demoInputBox"
+                    rows="3"></textarea>
+            </div>
+            <div>
+                <input type="submit" value="Send" class="btnAction" />
+            </div>
+        </form>
+        <div id="loader-icon" style="display: none;">
+            <img src="LoaderIcon.gif" />
+        </div>
 
 </div>
 
